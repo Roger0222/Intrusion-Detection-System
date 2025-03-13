b46e67a885bd9884ea591f3cce66c4e0ad7f7a4d
Roger0222 <20211113@nbsc.edu.ph>
2025-03-14 01:14:15 +0800

    added hidden credentials on send.py

--- send.py
+++ send.py
@@ -1,11 +1,23 @@
+import os
 from twilio.rest import Client
-account_sid = 'AC0a9e572fd75b2afa716d207774f5573d'
-auth_token = '67ac279cfdb9c054af2d5069ed39c7fa'
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get Twilio credentials from environment variables
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+
 client = Client(account_sid, auth_token)
+
 def sendSms():
     message = client.messages.create(
         from_='+19842764295',
         body='ALERT!!!',
-        to='+639632618615'
+        to='+my num'
     )
     print("SMS sent successfully:", message.sid)
+
+# Call the function to send SMS
+sendSms()
